59c075d79209903c95236cd840d938b9a20e88f4
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-03 14:11:56 +0800

    fix: package.json is not commited

--- release.config.js
+++ release.config.js
@@ -2,13 +2,9 @@ module.exports = {
   "plugins": [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ["@semantic-release/changelog", {
-      "changelogFile": "CHANGELOG.md",
-    }],
-    ["@semantic-release/git", {
-      "assets": ["CHANGELOG.md", "package.json"],
-    }],
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
+    '@semantic-release/git',
   ]
 }
