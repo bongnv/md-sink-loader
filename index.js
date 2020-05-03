9d08197a6997248f244b782519b7e7b2eaa7219b
Bong Nguyen <van.bong@grabtaxi.com>
2020-05-03 13:49:07 +0800

    ci: configure semantic-release

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function (source, map, meta) {
   const exported = JSON.stringify({
     ...meta.attributes,
     content: meta.html,
-  });
-  return `module.exports=${exported}`;
-};
+  })
+  return `module.exports=${exported}`
+}
---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    ["@semantic-release/github", {
+      "assets": [
+        {"path": "package.json", "label": "Version bump"},
+      ]
+    }]
+  ]
+}
